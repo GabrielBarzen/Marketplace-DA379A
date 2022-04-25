7daa85b41b992d2d920a335fa14196edadaff93a
Gabriel Modin Bärzén <gabriel.barzen@mailfence.com>
2022-04-25 15:12:10 +0200

    refined marketplace webpage, stub for product

--- marketplace/src/main/java/com/life/marketplace/presentation/MarketplaceController.java
+++ marketplace/src/main/java/com/life/marketplace/presentation/MarketplaceController.java
@@ -5,26 +5,29 @@ import com.life.marketplace.model.Products;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
+import java.util.Calendar;
 
 
 @RestController()
 //@RequestMapping(value = "/api/v01")
 public class MarketplaceController {
 
-
+    int testid = 0;
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     public String getProducts() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1999,Calendar.OCTOBER,7);
         Products product = new Products();
-        product.setId("01");
+        product.setId(String.valueOf(testid++));
         product.setName("macbook");
         product.setPrice(20000000);
-        product.setDate(new Date(1999,10,07));
+        product.setDate(new Date(calendar.getTimeInMillis()));
         product.setType("electronics");
         product.setColor("green");
-        product.setCondition("whack");
-        product.setStatus("Not sold its broken what did you expect");
-        product.setSeller("you :eyesemoji:");
-
+        product.setCondition("broken");
+        product.setStatus("Not sold, its broken");
+        product.setSeller("you \n" +
+                "\uD83D\uDC40 ");
         Gson gson = new Gson();
         return gson.toJson(product);
     }
