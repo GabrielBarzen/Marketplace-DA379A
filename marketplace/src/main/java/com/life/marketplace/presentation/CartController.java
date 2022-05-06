d469af07d6c9442dd20f246a59f568228d55ec7a
Gabriel Modin Bärzén <gabriel.barzen@mailfence.com>
2022-05-06 14:46:05 +0200

    loginpage bound submit button

---
+++ marketplace/src/main/java/com/life/marketplace/presentation/CartController.java
@@ -0,0 +1,7 @@
+package com.life.marketplace.presentation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class CartController {
+}
--- marketplace/src/main/resources/static/js/login.js
+++ marketplace/src/main/resources/static/js/login.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
     $("#header").load("/header.html");
 });
+
+
+
+function loginButtonPressed() {
+    let usernameInput = document.getElementById("usernameInput");
+    let passwordInput = document.getElementById("passwordInput");
+    loginUser(usernameInput.value, passwordInput.value);
+}
+
+function loginUser(username, password) {
+    console.log("logging in : " + username + " with password : " + password);
+    $.ajax({url: "/login/username="+username+"&password=" + password,
+        method: "POST",
+        headers: {"Accept": "application/json"},
+        });
+}
